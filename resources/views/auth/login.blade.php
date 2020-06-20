ab2dac5cdcbc9be67f0ba58c7c58860536177675
Faiz Hidayatulloh <faiz14hiedayatoelloh@gmail.com>
2020-06-20 16:43:22 +0700

    fix typo :v

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -52,6 +52,6 @@
         </div>
     </div>
     <div class="mt-5 text-muted text-center">
-        Don't have an account? <a href="{{ route('register') }}">{{  Create One  }}}}</a>
+        Don't have an account? <a href="{{ route('register') }}">{{  __('Create One')  }}</a>
     </div>
 @endsection
