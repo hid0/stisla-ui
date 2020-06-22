65858ef39107bf5eefa2721784bfba5abc0ecb5e
Faiz Hidayatulloh <faiz14hiedayatoelloh@gmail.com>
2020-06-22 14:18:53 +0700

    csrf token in auth.blade.php

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
   <title>@yield('title') &mdash; {{ env('APP_NAME') }}</title>
+  <meta name="csrf-token" content="{{ csrf_token() }}">
   <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 
